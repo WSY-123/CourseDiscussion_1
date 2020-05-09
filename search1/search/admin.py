4b76990145c9bebe363e65c36439dfca3d28eb1a
Frostmurne <1599101385@qq.com>
2020-05-10 00:31:47 +0800

    添加了登录注册功能

--- search1/search/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from search.models import lessons, feedback
-
-# Register your models here.
-admin.site.register(lessons)
-admin.site.register(feedback)
--- search1/search/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import feedback
-
-class FeedbackForm(forms.ModelForm):
-    class Meta:
-        model = feedback
-        labels = {'title': '标题', 'text': '内容'}
-        fields = ['title', 'text']
-        widgets = {'text': forms.Textarea(attrs={'col':80})}
