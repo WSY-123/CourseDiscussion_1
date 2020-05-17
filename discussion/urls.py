2fa43373bce19ec1307bb303904eee15543e22f4
Frostmurne <1599101385@qq.com>
2020-05-17 11:28:30 +0800

    添加了针对问题的搜索功能

--- discussion/urls.py
+++ discussion/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-
+from haystack.views import SearchView
 app_name = 'discuss'
 urlpatterns = [
     path('', views.index, name='index'),
     path('ask/', views.ask_question, name='ask_question'),
     path('tags/<int:id>/', views.tag_questions, name='tag_questions'),
     path('tags/add/', views.add_tag, name='add_tag'),
-    path('<int:id>/', views.question_details, name='question_details')
+    path('<int:id>/', views.question_details, name='question_details'),
+    path('search/', SearchView(), name='haystack_search'),
 ]
